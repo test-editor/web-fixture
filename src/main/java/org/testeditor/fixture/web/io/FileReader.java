880bce9032d79dceaa63b8671cba28c4c8e8fa94
Ersin Bayraktar <ersin.koray.bayraktar@signal-iduna.de>
2017-11-14 10:50:16 +0100

    FileReader incl. test

---
+++ src/main/java/org/testeditor/fixture/web/io/FileReader.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2012 - 2017 Signal Iduna Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Signal Iduna Corporation - initial API and implementation
+ * akquinet AG
+ * itemis AG
+ *******************************************************************************/
+
+package org.testeditor.fixture.web.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testeditor.fixture.web.WebDriverFixture;
+
+import com.google.common.io.CharStreams;
+
+public class FileReader {
+    
+    private static final Logger logger = LoggerFactory.getLogger(WebDriverFixture.class);
+
+
+    /**
+     * Opens a file defined as fileName and read the content line by line.
+     * 
+     * @return File content as String
+     */
+    public String getFileContentAsString(String fileName) {
+
+        // Get file from resources folder
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+       
+        String result = null;
+        try {
+            result = CharStreams.toString(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            logger.error("The file with the name {} can not be read in the resource folder. {}" , fileName, e);
+        } catch (NullPointerException e) {
+            logger.error("The file with the name {} can not be found in the resource folder. {}", fileName, e);
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/org/testeditor/fixture/web/io/FileReaderTest.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2012 - 2017 Signal Iduna Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Signal Iduna Corporation - initial API and implementation
+ * akquinet AG
+ * itemis AG
+ *******************************************************************************/
+
+package org.testeditor.fixture.web.io;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FileReaderTest {
+
+    static final String lineSeparator = System.getProperty("line.separator");
+        
+    String result = "firefox-cap1 = \"abcdüöß\"" + lineSeparator  
+            + "firefox-cap2 = 8" + lineSeparator  
+            + "firefox-cap3 = true";
+
+    @Test
+    public void testReadFile() throws Exception {
+        FileReader reader = new FileReader();
+        String fileContentAsString = reader.getFileContentAsString("utf8EncodedTextWithUmlaut.txt");
+        Assert.assertEquals(result, fileContentAsString);
+    }
+
+}
