49aa33077b84e41c17dfc3e900d02817ba090503
Ersin Bayraktar <ersin.koray.bayraktar@signal-iduna.de>
2017-11-30 15:32:09 +0100

    Modul Tests

--- src/main/java/org/testeditor/fixture/web/json/BrowserSettingsManager.java
+++ src/main/java/org/testeditor/fixture/web/json/BrowserSettingsManager.java
@@ -20,7 +20,10 @@ import org.openqa.selenium.Platform;
 
 public class BrowserSettingsManager {
 
+    private static final String CAPABILITIES = "capabilities";
+    private static final String OPTIONS = "options";
     private static final String FILE_NAME = "browserSetup.json";
+    
 
     /**
      * 
@@ -43,33 +46,112 @@ public class BrowserSettingsManager {
         
         BrowserSetupReader reader = new BrowserSetupReader();
         List<BrowserSetupElement> elements = reader.readElements(FILE_NAME);
-
+        
         Platform currentPlattform = Platform.getCurrent();
+        checkRedundantElements(elements,currentPlattform);
         return getBrowserSpecificSetting(elements, currentPlattform);
-
     }
     
+    /**
+     * This method is for testing purposes.
+     * @param fileName 
+     *          of JSON test file for browserSetup.json.
+     * @return 
+     *          A List of BrowserSetupElement
+     */
+    protected static List<BrowserSetupElement> getBrowserSettings(String fileName) {
+        
+        BrowserSetupReader reader = new BrowserSetupReader();
+        List<BrowserSetupElement> elements = reader.readElements(fileName);
+        
+        Platform currentPlattform = Platform.getCurrent();
+        checkRedundantElements(elements,currentPlattform);
+        return getBrowserSpecificSetting(elements, currentPlattform);
+    }
     
-    private static List<BrowserSetupElement> getBrowserSpecificSetting(List<BrowserSetupElement> 
+    protected static void checkRedundantElements(List<BrowserSetupElement> elements, Platform platform) {
+        List<BrowserSetting> duplicateSettings = new ArrayList<>();
+        
+        List<BrowserSetupElement> separatedBrowserSettingsForOs = separateBrowserSettingsForOs(elements, platform);
+        List<BrowserSetupElement> separateBrowserSettingsOsUnspecific = separateBrowserSettingsOsUnspecific(elements);
+        List<BrowserSetting> capabilitiesOsUnspecific = getBrowserSettingList(separateBrowserSettingsOsUnspecific,
+            CAPABILITIES); 
+        List<BrowserSetting> capabilitiesOsSpecific = getBrowserSettingList(separatedBrowserSettingsForOs,
+            CAPABILITIES); 
+        List<BrowserSetting> optionsOsUnspecific = getBrowserSettingList(separateBrowserSettingsOsUnspecific, 
+            OPTIONS);  
+        List<BrowserSetting> optionsOsSpecific = getBrowserSettingList(separatedBrowserSettingsForOs, 
+            OPTIONS);  
+        
+        // Look for duplicate capability entries and exit test when found. 
+        determineDuplicateEntries(duplicateSettings, capabilitiesOsUnspecific, capabilitiesOsSpecific);
+        // Look for duplicate option entries and exit test when found.
+        determineDuplicateEntries(duplicateSettings, optionsOsUnspecific, optionsOsSpecific);
+        
+        if (duplicateSettings.size() > 0) {
+            throw new RuntimeException("Duplicate entries existing in configuration file " 
+                + FILE_NAME + ". Entries are: " + duplicateSettings);
+        }
+    }
+    
+    protected static void determineDuplicateEntries(List<BrowserSetting> duplicateSettings, 
+            List<BrowserSetting> settingOsUnspecific, List<BrowserSetting> settingOsSpecific) {
+        for (BrowserSetting browserSetting : settingOsUnspecific) {
+            String capabilityName = browserSetting.getKey();
+            for (BrowserSetting browserSettingDuplicate : settingOsSpecific) {
+                String duplicateCapabilityName = browserSettingDuplicate.getKey();
+                if (capabilityName.equalsIgnoreCase(duplicateCapabilityName)) {
+                    duplicateSettings.add(browserSetting);
+                    duplicateSettings.add(browserSettingDuplicate);
+                } 
+            }
+        }
+    }
+    
+    protected static List<BrowserSetting> getBrowserSettingList(List<BrowserSetupElement> 
+        separateBrowserSettingsOsUnspecific, String browserSettingType) {
+        List<BrowserSetting> browserSettingList = new ArrayList<>();
+        separateBrowserSettingsOsUnspecific.forEach((bsetting) -> { 
+            if (browserSettingType.equals(OPTIONS)) {
+                bsetting.getOptions().forEach((option) ->  { 
+                    BrowserSetting setting = new BrowserSetting(option.getKey(), option.getValue());
+                    browserSettingList.add(setting);
+                } 
+                );
+            
+            } else if (browserSettingType.equals(CAPABILITIES)) {
+                bsetting.getCapabilities().forEach((capability) ->  { 
+                    BrowserSetting setting = new BrowserSetting(capability.getKey(), capability.getValue());
+                    browserSettingList.add(setting);
+                    } 
+                );
+            }  
+        
+        }   
+        ); 
+        return browserSettingList;
+    }
+    
+    protected static List<BrowserSetupElement> getBrowserSpecificSetting(List<BrowserSetupElement> 
         elements,Platform platform) {
         List<BrowserSetupElement> settings = new ArrayList<>();    
         settings = separateBrowserSettingsForOs(elements, platform);
-        separateBrowserSettingsOsUnspecific(settings, elements);
+        settings.addAll(separateBrowserSettingsOsUnspecific(elements));
         return settings;
     }
 
-    private static void separateBrowserSettingsOsUnspecific(List<BrowserSetupElement> settings,
-            List<BrowserSetupElement> elements) {
+    protected static List<BrowserSetupElement> separateBrowserSettingsOsUnspecific(List<BrowserSetupElement> elements) {
+        List<BrowserSetupElement> settings = new ArrayList<>();
         elements.forEach((browserSetupElement) -> {
             String osName = browserSetupElement.getOsName();
             if (osName == null || osName.isEmpty()) {
                 settings.add(browserSetupElement);
             }
         });
-        
+        return settings;
     }
 
-    private static List<BrowserSetupElement> separateBrowserSettingsForOs(List<BrowserSetupElement> elements,
+    protected static List<BrowserSetupElement> separateBrowserSettingsForOs(List<BrowserSetupElement> elements,
             Platform platform) {
         List<BrowserSetupElement> settings = new ArrayList<>();
         elements.forEach((browserSetupElement) -> {
---
+++ src/test/java/org/testeditor/fixture/web/json/BrowserSettingsManagerTest.java
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ * Copyright (c) 2012 - 2017 Signal Iduna Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Signal Iduna Corporation - initial API and implementation
+ * akquinet AG
+ * itemis AG
+ *******************************************************************************/
+
+
+package org.testeditor.fixture.web.json;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.openqa.selenium.Platform;
+
+public class BrowserSettingsManagerTest {
+    
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    
+    
+    @Test
+    public void failureInDuplicateCapability() {
+        
+        // given
+        // BrowserSettingsmanager
+        
+        // when
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("Duplicate entries existing in configuration file browserSetup.json. Entries are: "
+                + "[my.cap - 8, my.cap - 5]");
+        BrowserSettingsManager.getBrowserSettings("browserSetupWithFailureCapability.json");
+        
+        // then
+        // expected RuntimeException will be thrown.
+    }
+    
+    @Test
+    public void failureInDuplicateOption() {
+        
+        // given
+        // BrowserSettingsmanager
+        
+        // when
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("Duplicate entries existing in configuration file browserSetup.json. Entries are: "
+                + "[network.proxy.type - 8, network.proxy.type - 5]");
+        BrowserSettingsManager.getBrowserSettings("browserSetupWithFailureOption.json");
+        
+        // then
+        // expected RuntimeException will be thrown.
+    }
+    
+    @Test
+    public void successfulOptions() {
+        
+        // given
+        // BrowserSettingsmanager
+        
+        // when
+        List<BrowserSetupElement> browserSettings = BrowserSettingsManager.getBrowserSettings(
+                "browserSetupWithOptions.json");
+        
+        // then
+        if (Platform.getCurrent().equals(Platform.VISTA)) {
+            Assert.assertEquals(3, browserSettings.size());
+            Assert.assertEquals("firefox", browserSettings.get(1).getBrowserName());
+            Assert.assertEquals("windows-firefox-section",browserSettings.get(1).getBrowserSetupName());
+            Assert.assertEquals(null, browserSettings.get(2).getOsName());
+            Assert.assertEquals("firefox-section-without-os",browserSettings.get(2).getBrowserSetupName());
+        } else if (Platform.getCurrent().equals(Platform.LINUX)) {
+            Assert.assertEquals(2, browserSettings.size());
+            Assert.assertEquals("firefox-section-without-os",browserSettings.get(0).getBrowserSetupName());
+            Assert.assertEquals("firefox", browserSettings.get(0).getBrowserName());
+            Assert.assertEquals("linux-firefox-section",browserSettings.get(1).getBrowserSetupName());
+            Assert.assertEquals("firefox", browserSettings.get(1).getBrowserName());
+        } else if (Platform.getCurrent().equals(Platform.MAC)) {
+            Assert.assertEquals(2, browserSettings.size());
+            Assert.assertEquals("firefox-section-without-os",browserSettings.get(0).getBrowserSetupName());
+            Assert.assertEquals("firefox", browserSettings.get(0).getBrowserName());
+            Assert.assertEquals("mac-firefox-section",browserSettings.get(1).getBrowserSetupName());
+            Assert.assertEquals("firefox", browserSettings.get(1).getBrowserName());
+        }
+    }    
+
+}
+
+
